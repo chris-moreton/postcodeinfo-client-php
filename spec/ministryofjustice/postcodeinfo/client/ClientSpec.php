d8a9a129944c00418a69bff87fce610d3f00dab7
Chris Moreton <chris@netsensia.com>
2015-07-06 21:39:10 +0100

    Add more Spec tests

--- spec/ministryofjustice/postcodeinfo/client/ClientSpec.php
+++ spec/ministryofjustice/postcodeinfo/client/ClientSpec.php
@@ -14,11 +14,28 @@ class ClientSpec extends ObjectBehavior
         $this->shouldHaveType('ministryofjustice\postcodeinfo\client\Client');
     }
 
-    function it_will_perform_a_postcode_lookup()
+    function it_will_get_a_postcode_object_from_a_lookup()
     {
         $this->beConstructedWith(file_get_contents('spec/api_key'), 'https://postcodeinfo-staging.dsd.io/');
         $this->lookupPostcode('SW195AL')->shouldBeAValidPostcodeObject();
+    }
+    
+    function it_will_get_the_uprn()
+    {
+        $this->beConstructedWith(file_get_contents('spec/api_key'), 'https://postcodeinfo-staging.dsd.io/');
+        $this->lookupPostcode('DL3 0UR')->getAddresses()[0]->getUprn()->shouldBe('10013312514');
+    }
+    
+    function it_will_get_the_thoroughfare_name()
+    {
+        $this->beConstructedWith(file_get_contents('spec/api_key'), 'https://postcodeinfo-staging.dsd.io/');
         $this->lookupPostcode('SW195AL')->getAddresses()[0]->getThoroughfareName()->shouldBe('CHURCH ROAD');
+    }
+    
+    function it_will_get_the_organisation_name()
+    {
+        $this->beConstructedWith(file_get_contents('spec/api_key'), 'https://postcodeinfo-staging.dsd.io/');
+        $this->lookupPostcode('DL3 0UR')->getAddresses()[0]->getOrganisationName()->shouldBe('ARGOS LTD');
     }
     
     public function getMatchers()
